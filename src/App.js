500d318d07340ff096e0d01911ad03da881565e9
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-07 15:13:32 +0530

    added routes for the home and Cart components in the app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<Home/>} />
+          <Route path="/cart" element={<Cart/>} />
         </Routes>
       </BrowserRouter>
     </div>

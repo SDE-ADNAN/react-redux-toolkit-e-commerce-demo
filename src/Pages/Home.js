c9711162f0b01269bedb73d0045fef4b69160cc2
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-07 15:54:49 +0530

    made the products component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Products from '../components/Products'
 
 const Home = () => {
   return (
     <div>
         <h2 className='heading'>Welcome to the redux toolkit store  </h2>
+        <section>
+            <h3>Products</h3>
+            <Products/>
+        </section>
     </div>
   )
 }

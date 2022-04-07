31a9e2e5fd9468b3eb71892c57cc1b0eff74f702
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-07 15:17:41 +0530

    created and added the Navbar component to the app component outside the routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Cart from "./Pages/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/cart" element={<Cart/>} />

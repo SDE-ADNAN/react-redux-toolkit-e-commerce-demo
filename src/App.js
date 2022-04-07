1c4123ce84ff4656321175975a7bf8edbac46808
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-07 15:50:08 +0530

    made the navbar with links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Cart from "./Pages/Cart";
+import './index.css'
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -10,8 +11,8 @@ function App() {
       <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route className='navLink' path="/" element={<Home/>} />
-          <Route className='navLink' path="/cart" element={<Cart/>} />
+          <Route className="navLink" path="/" element={<Home/>} />
+          <Route className="navLink" path="/cart" element={<Cart/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-        <span className='logo'>Redux Store</span>
-        <div>
-            <Link to="/"  >Home</Link>
-            <Link to='/cart'>Cart</Link>
-            <span className='cartCount'> Cart Items : 0 :</span>
-        </div>
+    <div style={{ display: "flex", alignItems: "space-between" }}>
+      <span className="logo">Redux Store</span>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/cart">Cart</Link>
+        <span className="cartCount"> Cart Items : 0 :</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

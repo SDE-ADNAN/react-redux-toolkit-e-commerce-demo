8e8c874d8a87af1e9db6ad4f87eb923d9b252b82
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-08 12:08:59 +0530

    made some ui changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div style={{ display: "flex", alignItems: "space-between" }}>
+    <div style={{ display: "flex", alignItems: "center",justifyContent:"space-between" }}>
       <span className="logo">Redux Store</span>
       <div>
         <Link to="/">Home</Link>

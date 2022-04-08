459376f7b99d2a715cf42883b769ff862a53455e
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-08 11:58:25 +0530

    made the card component for products (its ui)

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,11 +8,23 @@ const Products = () => {
       const res = await fetch('https://fakestoreapi.com/products');
       const data = await res.json();
       console.log(data);
+      setProducts(data);
     }
     fetchProducts();
   },[])
   return (
-    <div>Products</div>
+    <div className='productsWrapper'>
+      {products.map((product)=>{
+          return(<div className='card' key={product.id}>
+                <img src={product.image} alt={product.title}/>
+                <h4>{product.title}</h4>
+                <h5>{product.price}</h5>
+                <button className='btn'>Add to Cart</button>
+          </div>)
+      })
+      }
+
+    </div>
   )
 }
 

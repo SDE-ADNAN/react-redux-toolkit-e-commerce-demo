b32fd228fcdc89eecebf1473f34279343e09c36d
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-08 11:44:33 +0530

    fetched the data from fake products api

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React , {useState,useEffect} from 'react'
 
 const Products = () => {
+  const  [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    const fetchProducts = async ()=>{
+      const res = await fetch('https://fakestoreapi.com/products');
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchProducts();
+  },[])
   return (
     <div>Products</div>
   )

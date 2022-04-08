217e611fa1534a53e4d2b86a831202a386a1a2dc
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-08 16:05:59 +0530

    made the ui for the cart page

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useSelector ,useDispatch} from 'react-redux'
+import { remove } from '../Store/cartSlice'
 
 const Cart = () => {
+  const products = useSelector((state)=>state.cart)
+  const dispatch= useDispatch();
+  const handleRemove=(product)=>{
+      dispatch(remove(product.id))
+  }
   return (
-    <div>Cart</div>
+    <div className="productsWrapper">
+      {products.map((product) => {
+        return (
+          <div className="cartCard" key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h4>{product.title}</h4>
+            <h5>{product.price}</h5>
+            <button
+              onClick={() => {
+                handleRemove(product);
+              }}
+              className="btn"
+            >
+              Delete from  Cart
+            </button>
+          </div>
+        );
+      })}
+    </div>
   )
 }
 
